dbd26ff719692fad5a9f98db97b6878418e5765c
unknown <ananddey.nic@gmail.com>
2020-01-17 23:27:57 +0530

    Edit complete

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,10 +6,34 @@ use Illuminate\Http\Request;
 use App\User;
 class ProfilesController extends Controller
 {
-    public function index($user) {
+    public function __construct(){
+
+        $this->middleware('auth');
+    }
+    
+
+    public function index(\App\User $user) {
        
-        $user=User::findOrFail($user);
+        // $user=User::findOrFail($user);
         return view('profiles.index',compact('user'));
         
+    }
+
+    public function edit(\App\User $user) {
+
+        return view('profiles.edit',compact('user'));
+    }
+
+    public function update(\App\User $user) {
+
+        $data=request()->validate([
+
+            'title'=>'required',
+            'description'=>'required',
+            'url'=>'url',
+            'image'=>'',
+        ]);
+        $user->profile->update($data);
+        return redirect("/profile/{$user->id}");
     }
 }
